57644712e8a7ab86b2dbb10d3517e344660974c0
unknown <tcutlip08@gmail.com>
2019-07-31 22:50:47 -0400

    home page if fully responsive until 750px-1650+px. link on right hide/show content

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,30 @@
-                                 
+$(document).ready(function () {
+    var collageHeight = $("#Code").height();
+    var setMarginForContact = collageHeight + 150;
+
+    $("#contact").css({ "margin-top": setMarginForContact + "px" });
+});
+
+$("#home").on("click", function(){
+    $(".home").attr("style", "display: block");
+    $(".bio").attr("style", "display: none");
+    $(".myWork").attr("style", "display: none");
+});
+$("#bio").on("click", function(){
+    $(".home").attr("style", "display: none");
+    $(".bio").attr("style", "display: block");
+    $(".myWork").attr("style", "display: none");
+});
+$("#myWork").on("click", function(){
+    $(".home").attr("style", "display: none");
+    $(".bio").attr("style", "display: none");
+    $(".myWork").attr("style", "display: block");
+});
+
+$(window).resize(function () {
+    $("#log").append("<div>Handler for .resize() called.</div>");
+    var collageHeight = $("#Code").height();
+    var setMarginForContact = collageHeight + 150;
+
+    $("#contact").css({ "margin-top": setMarginForContact + "px" });
+});
